d51d07f10e8ded22de08ae34c3926f88933767bc
GibboK <gibbok.coding@gmail.com>
2017-08-12 07:50:00 +0200

    Add behaviour on click addOperator
    OnClick addOperator make visible operatorsList

---
+++ src/js/mobility.js
@@ -0,0 +1,57 @@
+﻿; (function (window) {
+    jQuery.fn.visible = function () {
+        return this.css('visibility', 'visible');
+    };
+
+    jQuery.fn.invisible = function () {
+        return this.css('visibility', 'hidden');
+    };
+
+    var component = {
+        init: function (componentId) {
+            this.id = componentId;
+            this.isVisible = true;
+            this.domNode = $(this.id);
+        },
+        show: function () {
+            if (!this.isVisible) {
+                $(this.domNode).show();
+            }
+        },
+        hide: function () {
+            if (!this.isVisible) {
+                $(this.domNode).hide();
+            }
+        },
+        onClick: function (cb) {
+            $(this.domNode).click(cb)
+        }
+    };
+
+
+
+    $(function () {
+        debugger
+        var operatorsList = Object.create(component);
+        operatorsList.init('#operators__list');
+        operatorsList.isVisible = false;
+        operatorsList.show = function () {
+            if (!this.isVisible) {
+                $(this.domNode).visible();
+            }
+        };
+        operatorsList.hide = function () {
+            if (!this.isVisible) {
+                $(this.domNode).invisible();
+            }
+        };
+
+        var addOperator = Object.create(component);
+        addOperator.init('#operators__add__icon');
+        addOperator.onClick(function (event) {
+            operatorsList.show();
+        });
+
+    });
+
+})(window);

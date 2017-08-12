3068fe2a7c7a61849bc848f2f8aa7a0b1d0bb465
GibboK <gibbok.coding@gmail.com>
2017-08-12 07:59:04 +0200

    Add on click operator open ticket dialog

--- src/js/mobility.js
+++ src/js/mobility.js
@@ -31,7 +31,9 @@
 
 
     $(function () {
-        debugger
+        /**
+         * remember to add W icon logo inside active operator in operators list
+         */
         var operatorsList = Object.create(component);
         operatorsList.init('#operators__list');
         operatorsList.isVisible = false;
@@ -50,6 +52,17 @@
         addOperator.init('#operators__add__icon');
         addOperator.onClick(function (event) {
             operatorsList.show();
+        });
+
+        var ticketDialog = Object.create(component);
+        ticketDialog.init('#ticket');
+        ticketDialog.isVisible = false;
+
+
+        var activeOperator = Object.create(component);
+        activeOperator.init('#operators__list__operator2');
+        activeOperator.onClick(function (event) {
+            ticketDialog.show();
         });
 
     });
